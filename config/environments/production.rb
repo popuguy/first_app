2dd5a256f6719024b2ab05873c3db3db1fbfd6a9
Paul Cantrell <paulcantr@gmail.com>
2013-08-07 19:03:05 -0700

    Fix all

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ FirstApp::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
+  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
